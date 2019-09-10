bc66f8ff5deec72ece2c6a1eb45e8dccd873354d
nseraphin <seraphinnina@gmail.com>
2019-09-10 17:03:54 -0400

    CSS header + début CSS menu, à compléter

--- api/vues/Vue.class.php
+++ api/vues/Vue.class.php
@@ -31,7 +31,9 @@ class Vue {
 		    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		    <meta name="description" content="">
 		    <meta name="viewport" content="width=device-width">
+			<link rel="stylesheet" href="../css/reset.css" type="text/css" media="screen">
 			<link rel="stylesheet" href="../css/var.css" type="text/css" media="screen">
+			<link rel="stylesheet" href="../css/header.css" type="text/css" media="screen">
 			<link rel="stylesheet" href="../css/flex.css" type="text/css" media="screen">
 		    <link rel="stylesheet" href="../css/main.css" type="text/css" media="screen">
 		    <!--<script src="../../js/plugins.js"></script>-->
@@ -49,10 +51,46 @@ class Vue {
 	public function afficheEntete() {
 		?>
 	<body>
-	    <main>
 	        <header class="appbar">
-	            <h1><a href="/art-public-mtl/api/">L'art public à Montréal</a></h1> 
-	        </header>
+
+	            <a class="logo" href="/art-public-mtl/api/"><img src="../img/icons/logoAP.png" alt="Logo Art public Montréal"></a>
+				<div class="icons">
+					<a class="search hidden" href="#"><img src="../img/icons/search_40px.svg" alt="Icone de recherche"></a>
+					<a class="langue" href="#">EN</a>
+					<a class="menuCubes hidden" href="#"><img src="../img/icons/menu.svg" alt="Icone d'ouverture du menu"></a>
+					<a class="fermerMenu" href="#"><img src="../img/icons/close.svg" alt="Icone de fermeture du menu"></a>
+				</div>
+				<nav class="menu ouvert">
+					<a class="lien" href="#">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/>
+					<path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/></svg>
+						<p>Oeuvres</p>
+					</a>
+					<a class="lien" href="#">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M12 3c-4.97 0-9 4.03-9 9s4.03 9 9 9c.83 0 1.5-.67 1.5-1.5 0-.39-.15-.74-.39-1.01-.23-.26-.38-.61-.38-.99 0-.83.67-1.5 1.5-1.5H16c2.76 0 5-2.24 5-5 0-4.42-4.03-8-9-8zm-5.5 9c-.83 0-1.5-.67-1.5-1.5S5.67 9 6.5 9 8 9.67 8 10.5 7.33 12 6.5 12zm3-4C8.67 8 8 7.33 8 6.5S8.67 5 9.5 5s1.5.67 1.5 1.5S10.33 8 9.5 8zm5 0c-.83 0-1.5-.67-1.5-1.5S13.67 5 14.5 5s1.5.67 1.5 1.5S15.33 8 14.5 8zm3 4c-.83 0-1.5-.67-1.5-1.5S16.67 9 17.5 9s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+						<p>Artistes</p>
+					</a>
+					<a class="lien" href="#">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24">
+					<path d="M21.71 11.29l-9-9c-.39-.39-1.02-.39-1.41 0l-9 9c-.39.39-.39 1.02 0 1.41l9 9c.39.39 1.02.39 1.41 0l9-9c.39-.38.39-1.01 0-1.41zM14 14.5V12h-4v3H8v-4c0-.55.45-1 1-1h5V7.5l3.5 3.5-3.5 3.5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+						<p>Parcours</p>
+					</a>
+					<a class="lien" href="#">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/>
+					<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/></svg>
+						<p>À propos</p>
+					</a>
+					<a class="lien" href="#">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+						<p>Contact</p>
+					</a>
+					<a class="lien" href="#">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>						
+						<p>Compte</p>
+					</a>
+				</nav>	
+			</header>
+			<main>
 			
 		<?php
 		
@@ -66,12 +104,12 @@ class Vue {
 	 */
 	public function affichePied() {
 		?>
-	
+			</main>
 			<footer>
 				Certains droits réservés @ Jonathan Martel (2019)<br>
 				Sous licence Creative Commons (BY-NC 3.0)
 			</footer>
-		</main>
+		
 	</body>
 </html>
 
