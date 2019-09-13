eb5cd1fd0ee77f772579dda2497daa9f879824f5
nseraphin <seraphinnina@gmail.com>
2019-09-13 15:13:38 -0400

    Page accueil, CSS, slider des nouvelles oeuvres à finir (transitions...)+ adaptatif page

--- api/vues/Vue.class.php
+++ api/vues/Vue.class.php
@@ -44,6 +44,7 @@ class Vue {
 		    <!--<script src="../../js/plugins.js"></script>-->
 		    <script src="../js/main.js"></script>
 		    <script src="../js/menu.js"></script>
+		    <script src="../js/slider.js"></script>
 
 		</head>
 		<?php
@@ -179,7 +180,7 @@ class Vue {
 		</section>
 		<section class="nvlOeuvres">
 			<h2>Nouvelles oeuvres</h2>
-			<ul class="carrousel">
+			<!-- <ul class="carrousel"> -->
 				<!-- Générer les articles en php, structure HTML voulue : -->
 				<!-- <li><a href="lien vers la page de l'oeuvre"></a><img src="lien image de l'oeuvre" alt="Nom de l'oeuvre"></li> -->
 				<!-- test pour CSS, à supprimer -->
@@ -187,11 +188,53 @@ class Vue {
 				<!-- <li><a href="#"></a><img src="../img/oeuvres/1099_1.jpg" alt="Nom de l'oeuvre"></li> -->
 				<!-- <li><a href="#"></a><img src="../img/oeuvres/1119_1.jpg" alt="Nom de l'oeuvre"></li> -->
 				<!-- <li><a href="#"></a><img src="../img/oeuvres/1127_1.jpg" alt="Nom de l'oeuvre"></li> -->
-			</ul>
+			<!-- </ul> -->
 			<div class="points">
 
 			</div>
 			
+
+			<!-- TEST SLIDER -->
+			<!-- Slideshow container -->
+			<div class="slider">
+
+				<!-- Full-width images with number and caption text -->
+				<div class="slide fade">
+					<!-- <div class="numbertext">1 / 3</div> -->
+					<div class="img"></div>
+					<!-- <img src="../img/oeuvres/962_1.jpg"> -->
+					<!-- <div class="text">Caption Text</div> -->
+				</div>
+
+				<div class="slide fade">
+					<!-- <div class="numbertext">2 / 3</div> -->
+					<div class="img"></div>
+					<!-- <img src="../img/oeuvres/1099_1.jpg"> -->
+					<!-- <div class="text">Caption Two</div> -->
+				</div>
+
+				<div class="slide fade">
+					<!-- <div class="numbertext">3 / 3</div> -->
+					<div class="img"></div>
+					<!-- <img src="../img/oeuvres/1119_1.jpg"> -->
+					<!-- <div class="text">Caption Three</div> -->
+				</div>
+
+				<!-- Next and previous buttons -->
+				<a class="fleches prev">&#10094;</a>
+				<a class="fleches next">&#10095;</a>
+			</div>
+			<br>
+
+			<!-- The dots/circles -->
+			<div class="points">
+				<span class="point" data-slide="1"></span>
+				<span class="point" data-slide="2"></span>
+				<span class="point" data-slide="3"></span>
+			</div>
+
+
+
 		</section>
 		<?php
 		
