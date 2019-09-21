a11935f55cba6e02b1cad04faf57969df65d1a90
Cecilia <ziux23@hotmail.com>
2019-09-20 23:46:49 -0400

    Style liste oeuvres

--- api/vues/oeuvre/AfficheOeuvres.html.php
+++ api/vues/oeuvre/AfficheOeuvres.html.php
@@ -10,20 +10,20 @@
 						foreach ($aData as $cle => $oeuvre) {
 							extract($oeuvre);
 							?>
-							<section class="oeuvre carte">
-			                    <header class="image dummy">
-								<img src="/art-public-mtl/img/placeholder_640_480.jpg" />
-			                        <h2 class="titre"><?php echo $Titre?></h2> 
+							<section class="oeuvre conteneur_oeuvre_courante">
+			                    <header class="image dummy image_oeuvre_courante">
+									<h2 class="titre-oeuvre"><?php echo $Titre?></h2>
+									<a class="ouvrir-oeuvre" href="oeuvre/<?php echo $id_oeuvre ?>" data-link="/artPublic/api/oeuvre/<?php echo $id_oeuvre ?>/" data-id="<?php echo $id_oeuvre ?>"><img src="/art-public-mtl/img/placeholder_640_480.jpg" /></a>
 			                    </header>
-			                    <section class="texte">
-			                        <p class="description">
+			                    <section class="texte_pied_image">
+			                        <!-- <p class="description">
 			                            <?php echo $Description ?> 
-									</p>
+									</p> -->
 									<?php 
 									foreach($Artistes as $artiste){
 										extract($artiste);
 										?>
-										<p class="auteur">Par : <a href="artiste/<?php echo $id_artiste ?>">
+										<p class="auteur_liste_oeuvre"><a href="artiste/<?php echo $id_artiste ?>">
                            <?php 
                             if(isset($Nom) && $Nom!=""){
                                 echo $Nom .", ". $Prenom;
@@ -41,12 +41,12 @@
 									}
 
 									?>
-			                        <p class="arrondissement"><?php echo $Arrondissement?></p>
+			                        <p class="date_creation"><?php echo $dateCreation?></p>
 			                    </section>
-			                    <footer class="barre-action">
-								<a class="ouvrir-oeuvre" href="oeuvre/<?php echo $id_oeuvre ?>" data-link="/artPublic/api/oeuvre/<?php echo $id_oeuvre ?>/" data-id="<?php echo $id_oeuvre ?>">En savoir plus...</a>	
-								<!--<button class="ouvrir-oeuvre" data-link="/artPublic/api/oeuvre/<?php echo $id_oeuvre ?>/" data-id="<?php echo $id_oeuvre ?>">En savoir plus...</button>-->
-			                    </footer>
+			                    <!-- <footer class="barre-action">
+			
+								<!<button class="ouvrir-oeuvre" data-link="/artPublic/api/oeuvre/<?php echo $id_oeuvre ?>/" data-id="<?php echo $id_oeuvre ?>">En savoir plus...</button>
+			                    </footer> -->
 			                </section>
 							
 							
