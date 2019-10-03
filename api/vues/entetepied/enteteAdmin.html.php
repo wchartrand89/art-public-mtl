7f65054ad15b69782a0e8d01b7de2a23fc7c8833
Alejandro Rendon <larendonrestrepo@gmail.com>
2019-10-03 10:55:58 -0400

    Modification page Admin/AffichageOeuvres.html.php

--- api/vues/entetepied/enteteAdmin.html.php
+++ api/vues/entetepied/enteteAdmin.html.php
@@ -20,29 +20,16 @@
 			<link rel="stylesheet" href="../../css/var.css" type="text/css" media="screen">
 			<link rel="stylesheet" href="../../css/adminmain.css" type="text/css" media="screen">
 			<link rel="stylesheet" href="../../css/headerAdmin.css" type="text/css" media="screen">
-
-<!--			 <link rel="stylesheet" href="../../img/icons/infos.svg" type="text/css" media="screen"> -->
-
-
+			<link rel="stylesheet" href="../../css/footerAdmin.css" type="text/css" media="screen">
+			
 		</head>
 		<body>
 	    
-			<header class="appbar">
-				 
-<!--				<a class="logo" href="/art-public-mtl/api/"><img src="../../img/icons/logoAP.png" alt="Logo Art public Montréal"></a>	-->
-				<div class="icons">
-
-<!--					<a class="langue hidden" href="#"><p class="textNavAdmin">EN</p></a>-->
-<!--					<a class="info" href="#"><img src="../../img/icons/infos.svg" alt="Icone d'information"></a>-->
-<!--					<a class="" href="#"><p class="textNavAdmin">Déconnexion</p></a>-->
-
-            <form id='deconnexion' action="?action=deconnexion" method="post">	
-                <div>
-                    <input type="submit" id="envoyer" value="Déconnexion">
-                </div>		
-            </form>
+			<header class="appbar">				 
+				<a class="logo" href="/art-public-mtl/api/"><img src="../../img/icons/logoAP.png" alt="Logo Art public Montréal"></a>
+				<a class = "Inter" href="#">En</a>
+				<a class = "Deconn" href="#">Déconnexion</a>			
 				
-				</div>		
 			</header>
 			<main>
 
