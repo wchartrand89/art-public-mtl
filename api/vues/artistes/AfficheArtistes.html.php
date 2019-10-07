e744828f41ccc939da122701a890be555f1370e4
nseraphin <seraphinnina@gmail.com>
2019-10-07 15:28:07 -0400

    ajout d'une div avec id lettre dans liste artiste pour lien avec ancre dans liste deroulante

--- api/vues/artistes/AfficheArtistes.html.php
+++ api/vues/artistes/AfficheArtistes.html.php
@@ -20,6 +20,93 @@
 <section class="liste">
 <?php
 
+$listeLettres = array(
+	0=>array(
+	"lettre"=>"A",
+	 "ok"=>false),
+	1=>Array(
+	"lettre"=>"B",
+	 "ok"=>false),
+	2=>Array(
+	"lettre"=>"C",
+	 "ok"=>false),
+	3=>Array(
+	"lettre"=>"D",
+	 "ok"=>false),
+	4=>Array(
+	"lettre"=>"E",
+	 "ok"=>false),
+	5=>Array(
+	"lettre"=>"F",
+	 "ok"=>false),
+	6=>Array(
+	"lettre"=>"G",
+	 "ok"=>false),
+	7=>Array(
+	"lettre"=>"H",
+	 "ok"=>false),
+	8=>Array(
+	"lettre"=>"I",
+	 "ok"=>false),
+	9=>Array(
+	"lettre"=>"J",
+	 "ok"=>false),
+	10=>Array(
+	"lettre"=>"K",
+	 "ok"=>false),
+	11=>Array(
+	"lettre"=>"L",
+	 "ok"=>false),
+	12=>Array(
+	"lettre"=>"M",
+	 "ok"=>false),
+	13=>Array(
+	"lettre"=>"N",
+	 "ok"=>false),
+	14=>Array(
+	"lettre"=>"O",
+	 "ok"=>false),
+	15=>Array(
+	"lettre"=>"P",
+	 "ok"=>false),
+	16=>Array(
+	"lettre"=>"Q",
+	 "ok"=>false),
+	17=>Array(
+	"lettre"=>"R",
+	 "ok"=>false),
+	18=>Array(
+	"lettre"=>"S",
+	 "ok"=>false),
+	19=>Array(
+	"lettre"=>"T",
+	 "ok"=>false),
+	20=>Array(
+	"lettre"=>"U",
+	 "ok"=>false),
+	21=>Array(
+	"lettre"=>"V",
+	 "ok"=>false),
+	22=>Array(
+	"lettre"=>"W",
+	 "ok"=>false),
+	23=>Array(
+	"lettre"=>"X",
+	 "ok"=>false),
+	24=>Array(
+	"lettre"=>"Y",
+	 "ok"=>false),
+	25=>Array(
+	"lettre"=>"Z",
+	 "ok"=>false),
+	);
+?>
+
+
+
+
+<?php
+
 // VERSION OK SI ARTISTES pas bien classés (nom collectif/nom )
 // foreach ($aData as $cle => $artiste) {
 // 	extract($artiste);
@@ -39,7 +126,6 @@
 	
 // }
 
-
 $Noms=[];
 $i=0;
 foreach ($aData as $cle => $artiste) {
@@ -56,17 +142,33 @@ foreach ($aData as $cle => $artiste) {
 	$i++;
 }
 sort($Noms);
-
+$test=[];
 foreach ($Noms as $cle => $artiste) {
 	extract($artiste);
-		if(isset($nom) && $nom!=""){
-		echo '<a class="lienArtiste '.$nom[0].'" href="artiste/'.$id.'">'.$nom .'</a>';
-	}
-	else
-	{
-		echo '<a class="lienArtiste '.$nom[0].'" href="artiste/'.$id.'">'.$nom.'</a>' ;
-	}	
-}
+	$i=0;
+	foreach($listeLettres as $cle => $lettre){
+		if(!isset($test[$i])){
+			$test[$i]=false;
+		}
+		if ($nom[0] == $lettre["lettre"] && $lettre["ok"] == false && $test[$i] !== true){
+			echo '<div id="'.$lettre["lettre"].'"></div>';
+			$test[$i]=true;
+		}
+		$i++;
 
+	}
+	
+	if(isset($nom) && $nom!=""){
+		
+	echo '<a class="lienArtiste '.$nom[0].'" href="artiste/'.$id.'">'.$nom .'</a>';
+}
+else
+{
+	echo '<a class="lienArtiste '.$nom[0].'" href="artiste/'.$id.'">'.$nom.'</a>' ;
+}	
+	
+	
+}
+//var_dump($Noms);
 ?>
 </section>
