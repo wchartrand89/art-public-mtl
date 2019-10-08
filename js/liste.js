df5fd2eba77ae979c45043973e268bf07123a589
nseraphin <seraphinnina@gmail.com>
2019-10-08 09:41:45 -0400

    liste lettres finie : clic sur fleche change lettres et lien ancre sur la page

--- js/liste.js
+++ js/liste.js
@@ -41,37 +41,42 @@ window.addEventListener("load", function(){
 
                 //changer le lien des fleches en fonction de celle sur laquelle on a cliqué
                 console.log(adresse);
+                // if(fleche.classList.contains("next")){
+                //     if(adresse ==0){
+                //         flecheNext.href="#"+listeLettres[adresse+25];
+                //         flechePrev.href="#"+listeLettres[adresse];
+                //         adresse=25;
+                //     }else if(adresse==25){
+                //         flechePrev.href="#"+listeLettres[25];
+                //         adresse= 24;
+                //     }
+                //     else{
+                //         flecheNext.href="#"+listeLettres[adresse-1];
+                //         flechePrev.href="#"+listeLettres[adresse];
+                //         adresse =adresse-1;
+                //     }
+                // }else{
+                //     if(adresse ==0){
+                //         // flecheNext.href="#"+listeLettres[adresse];
+                //         flechePrev.href="#"+listeLettres[adresse+2];
+                //         adresse++;
+                //     }else if(adresse==24){
+                //         flechePrev.href="#"+listeLettres[0];
+                //         adresse++;
+                //     }else if(adresse==25){
+                //         flechePrev.href="#"+listeLettres[1];
+                //         adresse=0;
+                //     }
+                //     else{
+                //         // flecheNext.href="#"+listeLettres[adresse-1];
+                //         flechePrev.href="#"+listeLettres[adresse+2];
+                //         adresse++;
+                //     }
+                // }
                 if(fleche.classList.contains("next")){
-                    if(adresse ==0){
-                        flecheNext.href="#"+listeLettres[adresse+25];
-                        flechePrev.href="#"+listeLettres[adresse];
-                        adresse=25;
-                    }else if(adresse==25){
-                        flechePrev.href="#"+listeLettres[25];
-                        adresse= 24;
-                    }
-                    else{
-                        flecheNext.href="#"+listeLettres[adresse-1];
-                        flechePrev.href="#"+listeLettres[adresse];
-                        adresse =adresse-1;
-                    }
+                    flecheNext.href="#"+document.querySelector(".focus").innerHTML;
                 }else{
-                    if(adresse ==0){
-                        // flecheNext.href="#"+listeLettres[adresse];
-                        flechePrev.href="#"+listeLettres[adresse+2];
-                        adresse++;
-                    }else if(adresse==24){
-                        flechePrev.href="#"+listeLettres[0];
-                        adresse++;
-                    }else if(adresse==25){
-                        flechePrev.href="#"+listeLettres[1];
-                        adresse=0;
-                    }
-                    else{
-                        // flecheNext.href="#"+listeLettres[adresse-1];
-                        flechePrev.href="#"+listeLettres[adresse+2];
-                        adresse++;
-                    }
+                    flechePrev.href="#"+document.querySelector(".focus").innerHTML;
                 }
                 console.log(adresse);
         });
