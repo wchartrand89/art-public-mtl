0dcf8c35aee526d5f14a1357571954cc2df74dfd
Cecilia <ziux23@hotmail.com>
2019-10-10 08:32:53 -0400

    Pages À propos et Contact

---
+++ api/controlleurs/ContactControlleur.class.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Controlleur de la ressource Contact
+ * 
+ * 
+ * @author Jonathan Martel
+ * @version 1.0
+ * @update 2016-11-25
+ * @license Creative Commons BY-NC 3.0 (Licence Creative Commons Attribution - Pas d’utilisation commerciale 3.0 non transposé)
+ * @license http://creativecommons.org/licenses/by-nc/3.0/deed.fr
+ */
+ 
+ /*
+ * 
+ *
+ */
+
+ 
+ 
+class ContactControlleur extends Controlleur 
+{
+	
+	// GET : 
+	// 		/contact/ - Contact
+	
+	
+	public function getAction(Requete $requete)
+	{
+		$oVue = new Vue();
+        $oVue->afficheEntete("");	
+        $oVue->afficheContact();
+        $oVue->affichePied();
+	}
+	
+}
+?>
