4226d426c9cdda330ecfd16b4efd22240b21360e
nseraphin <seraphinnina@gmail.com>
2019-10-10 11:35:25 -0400

    clique sur type d'oeuvres : tableau envoyé ok + changement des chackbox pour montrer quels critères sont choisis ou non

--- js/filtres.js
+++ js/filtres.js
@@ -7,8 +7,15 @@ window.addEventListener("load", function(){
     //console.log(filtresType);
     filtresType.forEach(function(type){
         type.addEventListener("click", function(){
-            
-            aTypes= aCheck(aTypes, type.dataset.id);
+            let checkbox=type.firstElementChild;
+            let check= aCheck(aTypes, type.dataset.id);
+            if(check === false){
+                aTypes.push(type.dataset.id);
+                checkbox.innerHTML="check_box";
+            }else{
+                remove(aTypes, check);
+                checkbox.innerHTML="check_box_outline_blank";
+            };
             ajax(aTypes);
             console.log(aTypes);
         });
@@ -43,22 +50,28 @@ window.addEventListener("load", function(){
     l'ajoute si il n'y était pas.
     @param {array} tableau : le tableau à vérifier
     @param {integer} id : l'id à insérer ou retirer du tableau
-    @return {array} tableauUp : la tableau updaté
+    @return {bool/integer} index : false si l'id n'existe pas dans le tableau, ou sa position dans le tableau
     */
     function aCheck(array, id){
         let test =false;
+        let index =false;
         if(!isNaN(id)){
             for(i=0; i<array.length; i++){
-                if(array[i]==id){
-                    array.splice(i, 1);
+                if(array[i]===id){
                     test= true;
+                    index= i;
                 }
             }
-            if(!test){
-                array.push(id);
-            }
         }
-        return array;
+        return index;
+    }
+    /*Fonction à laquelle on envoi un tableau et un entier, 
+        qui enlève du tableau l'élément à la position de l'entier envoyé
+        @param {array} array : le tableau à manipuler
+        @param {integer} i : la position de l'élément à supprimer dans le tableau
+        */
+    function remove(array, i){
+        array.splice(i, 1);
     }
 
 
