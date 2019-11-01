a5e3673e3f4e723a6b5de02b2d9e1d07be3315eb
Chartrand <e0637883@cmaisonneuve.qc.ca>
2019-10-31 20:29:42 -0400

    changements finaux langue accueil carte contact etc

--- api/vues/entetepied/entete.html.php
+++ api/vues/entetepied/entete.html.php
@@ -40,8 +40,6 @@
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Pacifico&display=swap" rel="stylesheet">
-
-	history
 	<?php
 		if ($page== ""){			
 			cssBase("");
